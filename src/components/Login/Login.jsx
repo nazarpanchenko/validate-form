59479a59826b1edc34f769d3f589f3ea9b2f4a98
Nazar <panchenko.nazar.a@gmail.com>
2021-02-19 13:21:14 +0200

    initialized, created basic projexct structure

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+
+const Login = () => {
+    return (
+        <main>
+            <div className="container"></div>
+        </main>
+    );
+};
+
+export default Login;
