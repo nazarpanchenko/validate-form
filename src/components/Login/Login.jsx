a5b0af280c087ab1992e7fe53d4bb5ca22753d50
Nazar <panchenko.nazar.a@gmail.com>
2021-02-19 14:27:11 +0200

    added basic layout ands styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
+import './login.scss';
 
 const Login = () => {
+    const backgroundImage = {
+        backgroundIimage: `url('../../assets/bg.jpg'), url('../../assets/bottom-bg.jpg')`,
+        backgroundPosition: `center center, bottom left`,
+        backgroundSize: `cover, cover`,
+        backgroundAttachment: `fixed, fixed`,
+        backgroundRepeat: `no-repeat, no-repeat`,
+        backgroundColor: `#eef2f5`
+    };
+
     return (
         <main>
-            <div className="container"></div>
+            <FormControl style={backgroundImage}>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">
+                    We'll never share your email.
+                </FormHelperText>
+            </FormControl>
         </main>
     );
 };
